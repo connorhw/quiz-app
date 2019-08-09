1c36a87ae8458d1dc49cab030ec3e9870e93cc30
Connor <connorwong93@gmail.com>
2019-08-09 13:40:47 -0700

    Finished the questions page wireframe. Button is functioning now

---
+++ datastore.js
@@ -0,0 +1,14 @@
+const STORE = {
+    {
+        question: 'What year was the BAA renamed to the NBA?',
+        answers: {
+            ''
+            ''
+            ''
+            '',
+        }
+    }
+    {
+
+    }
+}
