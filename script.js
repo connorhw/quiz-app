9c38d4b222b20ef77ec992c4dc871db06514f340
Connor <connorwong93@gmail.com>
2019-08-13 12:41:11 -0700

    added index1 and index2 to show all wireframes

--- script.js
+++ script.js
@@ -8,8 +8,22 @@ function startQuiz () {
   
   function generateQuestion (){
     return `<div class="question">
-      <h2>Question1</h2>
-    </div>`
+      <h1>Question1</h1>
+    </div>
+    <div class="answer">
+      <h3>Option1</h3>
+    </div>
+    <div class="answer">
+      <h3>Option2</h3>
+    </div>
+    <div class="answer">
+      <h3>Option3</h3>
+    </div>
+    <div class="answer">
+      <h3>Option4</h3>
+    </div>
+  
+    <button type="button" class="nextButton">NEXT</button>`
   }
   function renderQuestion () {
     $('.questionAnswerForm').html(generateQuestion());
