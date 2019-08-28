dce8f1132ee13649690390247f7704edeaa49682
Connor <connorwong93@gmail.com>
2019-08-27 22:56:27 -0700

    Committing everything in repl

--- script.js
+++ script.js
@@ -1,44 +1,60 @@
-/*
-function startQuiz () {
-    $('.startQuiz').on('click', '.startButton', function (event) {
-      $('.startQuiz').remove();
-      $('.questionAnswerForm').css('display', 'block');
-      //$('.questionNum').text(1);
-  });
+const STORE = [
+  {
+    question: 'What year was the BAA renamed to the NBA?',
+    answers: [
+      '1949',
+      '1959',
+      '1969',
+      '1979'
+    ]
+  },
+  {
+    question: 'Whom was the Commissioner before Adam Silver?',
+    answers: [
+      'Jerry West',
+      'Larry O Brien',
+      'David Stern',
+      'No one'
+    ]
   }
-  
-  function generateQuestion (){
-    return `<div class="question">
-      <h1>Question1</h1>
-    </div>
-    <div class="answer">
-      <h3>Option1</h3>
-    </div>
-    <div class="answer">
-      <h3>Option2</h3>
-    </div>
-    <div class="answer">
-      <h3>Option3</h3>
-    </div>
-    <div class="answer">
-      <h3>Option4</h3>
-    </div>
-  
-    <button type="button" class="nextButton">NEXT</button>`
-  }
-  function renderQuestion () {
-    $('.questionAnswerForm').html(generateQuestion());
-  }
-  
-  function createQuiz () {
-    startQuiz();
-    renderQuestion();
-    //userSelectAnswer();
-    //renderNextQuestion();
-  }
-  
-  $(createQuiz);
-  
-  */
+];
+let questionNumber = 0;
+function startQuiz (){ 
+  $('.startQuiz').on('click', '.startButton', function (event) {
+    $('.startQuiz').remove();
+    $('.quizForm').css('display', 'block');
+    //$('.questionNumber').text(1);
+});
+}
 
-  
+function generateQuestion(){
+
+  return `<div class="question">
+    ${STORE[questionNumber].question}
+  </div></br>
+  <div class="answers">
+    <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required> 
+    <span>${STORE[questionNumber].answers[questionNumber]}</span></br>
+    <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required>
+    <span>${STORE[questionNumber].answers[1]}</span></br>
+    <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required> 
+    <span>${STORE[questionNumber].answers[2]}</span></br>
+    <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required> 
+    <span>${STORE[questionNumber].answers[3]}</span></br>
+    </br>
+    <button type="submit" class="nextButton">Next</button>
+  </div>`
+}
+
+function renderQuestion(){
+  $('.quizForm').html(generateQuestion());
+}
+
+function createQuiz () {
+  startQuiz();
+  renderQuestion();
+  //userSelectAnswer();
+  //renderNextQuestion();
+}
+
+$(createQuiz);
