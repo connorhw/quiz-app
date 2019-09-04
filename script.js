23cc9cb0023b0d58dc3749e66ef687695038117b
Connor <connorwong93@gmail.com>
2019-09-03 18:31:05 -0700

    added feedback page to js, instead of having its own html page

--- script.js
+++ script.js
@@ -57,22 +57,41 @@ function startQuiz (){
 }
 
 function generateQuestion(){
+  if (questionNumber < 5){
+      return `<div class="question">
+        ${STORE[questionNumber].question}
+      </div></br>
+      
+      <div class="answers">
 
-  return `<div class="question">
-    ${STORE[questionNumber].question}
-  </div></br>
-  <div class="answers">
-    <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required> 
-    <span>${STORE[questionNumber].answers[0]}</span></br>
-    <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required>
-    <span>${STORE[questionNumber].answers[1]}</span></br>
-    <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required> 
-    <span>${STORE[questionNumber].answers[2]}</span></br>
-    <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required> 
-    <span>${STORE[questionNumber].answers[3]}</span></br>
-    </br>
-    <button type="submit" class="nextButton">Next</button>
-  </div>`
+        <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required> 
+        <span>${STORE[questionNumber].answers[0]}</span></br>
+        <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required>
+        <span>${STORE[questionNumber].answers[1]}</span></br>
+        <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required> 
+        <span>${STORE[questionNumber].answers[2]}</span></br>
+        <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required> 
+        <span>${STORE[questionNumber].answers[3]}</span></br>
+        </br>
+        <button type="submit" class="nextButton">Next</button>
+      
+      </div>`
+   } else{
+     renderResults();
+   }
+}
+
+function generateResults(){
+  return `<div id="feedback"style="border: 1px solid grey;">
+            <h2>Here's some feedback:</h2>
+              <div id="correct" style="border: 10px solid green;">What you know (correct answers)</div>
+              <div id="incorrect" style="border: 10px solid red;">What you don't (incorrect answers)</div>
+            </div>
+            <button type="button" class="nextButton"><a href="results.html">NEXT(Results)</a></button>`
+}
+
+function renderResults(){
+  $('.quizForm').html(generateResults());
 }
 
 function renderQuestion(){
